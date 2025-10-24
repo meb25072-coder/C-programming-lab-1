634bf31fba6bd7e8a542595cf33ae196af0dc768
meb25072-coder <meb25072@tezu.ac.in>
2025-10-25 00:24:53 +0530

    Update simple_calculator.c

--- Lab_4/simple_calculator.c
+++ Lab_4/simple_calculator.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,op,sum,sub,div,multi;
+	float a,b,sum,sub,div,multi;
+	int op;
 	printf("enter the first number:");
-	scanf("%d",&a);
+	scanf("%f",&a);
 	printf("choose the operator\n(add)=1\n(sub)=2\n(div)=3\n(multi)=4\nenter the operator number:\n");
 	scanf("%d",&op);
 	printf("enter the second number:");
-	scanf("%d",&b);
+	scanf("%f",&b);
 	sum=a+b;
 	sub=a-b;
 	div=a/b;
@@ -16,16 +17,16 @@ int main()
 	switch (op)
 	{
 		case 1:
-			printf("the result is : %d",sum);
+			printf("the result is : %f",sum);
 			break;
 	    case 2:
-	    	printf("the result is :%d",sub);
+	    	printf("the result is :%f",sub);
 	    	break;
 	    case 3:
-		    printf("the result is :%d",div);
+		    printf("the result is :%f",div);
 			break;
 		case 4:
-		    printf("the result is :%d",multi);
+		    printf("the result is :%f",multi);
 			break;
 		default	:
 		   printf("invalid Input");
